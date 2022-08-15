7d522e38f4edbe8f00e2dae3edf8d118f3cd6007
YavuzKarakoc <yavuzkarakocsosyal@gmail.com>
2022-08-16 00:34:50 +0300

    NodeJs O8 add ejs

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path')
 
 const app = express();
 
-app.get('/', (req, res) =>{
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
-    res.send(blog)
+//template engine
+app.set("view engine", "ejs")
+
+app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+//   const blog = {
+//     id: 1,
+//     title: 'Blog title',
+//     description: 'Blog description',
+//   };
+//   res.send(blog);
+// });
+
+//Routes
+app.get('/',(req, res) =>{
+    // res.sendFile(path.resolve(__dirname, 'view/index.ejs'))
+    res.render('index')
 })
+app.get('/about',(req, res) =>{
+    res.render('about')
+})
+app.get('/add_post',(req, res) =>{
+    res.render('add_post')
+})
+
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Sunucu ${port} portunda başlatıldı..`)
-})
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
