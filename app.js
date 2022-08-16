4d1a5669e8c3986628cc9c20145f3ba5e8b1e7e7
YavuzKarakoc <yavuzkarakocsosyal@gmail.com>
2022-08-16 19:56:57 +0300

    NodeJs Ö10 add detail page

--- app.js
+++ app.js
@@ -22,9 +22,10 @@ mongoose.connect('mongodb://localhost/cleanblog-test-db', {
 //template engine
 app.set("view engine", "ejs")
 
+// MIDDLEWARES
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
 // app.get('/', (req, res) => {
 //   const blog = {id: 1, title: 'Blog title', description: 'Blog description',};
@@ -32,26 +33,33 @@ app.use(express.static('public'));
 // });
 
 //Routes
-app.get('/', async (req, res) =>{
+app.get('/', async (req, res) => {
     // res.sendFile(path.resolve(__dirname, 'view/index.ejs'))
     const photos = await Photo.find({});
     res.render('index', {
         photos
     })
 })
-app.get('/about',(req, res) =>{
+app.get('/about', (req, res) => {
     res.render('about')
 })
-app.get('/add_post',(req, res) =>{
+app.get('/add_post', (req, res) => {
     res.render('add_post')
 })
 app.post("/photos", async (req, res) => {
     await Photo.create(req.body);
     res.redirect("/");
-  });
+});
+
+app.get('/post/:id', async (req, res) => {
+    const photo = await Photo.findById(req.params.id)
+    res.render('post' ,{
+        photo
+    })
+})
 
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Sunucu ${port} portunda başlatıldı..`);
+    console.log(`Sunucu ${port} portunda başlatıldı..`);
 });
